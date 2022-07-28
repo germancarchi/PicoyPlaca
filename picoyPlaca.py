e033e2e7bb3b1c18813f1782b74d53f5fb0d5119
GermanCarchi <germancarchi@gmail.com>
2022-07-28 16:33:10 -0500

    Primer borrador

---
+++ picoyPlaca.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+import datetime
+
+now = datetime.now()
+"""
+
+placa = input("Por favor ingresa la placa de tu vehículo (ej. PBC4567): ") 
+dia = input("Por favor ingresa el día de la semana (lunes, martes, etc.): ")
+hora = input("Por favor ingresa la hora actual (ej. 16:45): ")
+
+hora = now.hour
+if 
+print (hora)
+"""
+
+
+fecha1 = datetime.datetime(2020, 4, 13)
+fecha2 = datetime.datetime(2020, 4, 13)
+print(fecha1 == fecha2)
+
+
+fecha3 = datetime.datetime(2020, 4, 13)
+print(fecha1 == fecha3)
